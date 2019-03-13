313202ba306a360709ced5683e4817c40e236384
Fernando Brandao <fwbrandao@gmail.com>
2019-03-13 08:41:26 +0000

    mongoDB

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -4,7 +4,6 @@ exports.signup = function(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
 
-
     User.findOne({ email: email }, function(err, existingUser){
 
     });
