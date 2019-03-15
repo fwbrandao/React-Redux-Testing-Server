4b16a4bc632694d23533a4558ad2e3b5b006a56a
Fernando Brandao <fwbrandao@gmail.com>
2019-03-15 08:46:01 +0000

    dcrypt

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -4,7 +4,31 @@ exports.signup = function(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
 
+    if (!email || !password) {
+        return res.status(422).send({error: 'Please provide email and password'});
+    }
+
     User.findOne({ email: email }, function(err, existingUser){
+        if (err) { return next(err);}
+
+        // If user does exist, return error
+        if (existingUser) {
+            return res.status(422).send({error: 'Email in use'});
+        }
+
+        // If user does not exist, return error
+        const user = new User({
+            email: email,
+            password: password
+        });
+
+        user.save(function(err) {
+            if (err) {
+                return next(err);
+            }
+        // User was created
+        res.json(user);
+        })
 
     });
 }
