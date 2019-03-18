5997218ccee4c51debb2122c4a1516dfbbe76117
Fernando Brandao <fwbrandao@gmail.com>
2019-03-18 08:32:20 +0000

    services/passport.js

--- services/passport.js
+++ services/passport.js
@@ -3,3 +3,19 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// Setup options for JWT Strategy
+const jwtOptions = {};
+
+// Create JWT strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+    User.findById(payload.sub, function(err, user) {
+        if (err) { return done(err, false); }
+
+        if(user) {
+            done(null, user);
+        } else {
+            done(null, false);
+        }
+    });
+});
