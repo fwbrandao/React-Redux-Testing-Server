694d64bffc0a211d5544b46d8ca30d931af3acaf
Fernando Brandao <fwbrandao@gmail.com>
2019-03-19 08:42:36 +0000

    signin

--- router.js
+++ router.js
@@ -1,11 +1,18 @@
-const Authentication = require('./controllers/authentication')
+const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
 
-    app.post('/signup', Authentication.signup);
-
     // HTTP GET REQUEST
-    app.get('/', function(req, res, next) {
+    app.get('/', requireAuth, function(req, res) {
         res.send(['a', 'b', 'c']);
     });
+
+    // HTTP POST REQUEST
+    app.post('/signin', requireSignin, Authentication.signin);
+    app.post('/signup', Authentication.signup);
 }
--- services/passport.js
+++ services/passport.js
@@ -3,9 +3,31 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+// Create local strategy
+const localOptions = { usernameField: 'email'};
+const localLogin = new LocalStrategy(localOptions, function(email, password, done){
+    User.findOne({ email: email }, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+
+        user.comparePassword(passport, function(err, isMatch) {
+            if (err) {
+                return done(err);
+            }
+            if (!isMatch) {
+                return done(null, false);
+            }
+        });
+    });
+});
 
 // Setup options for JWT Strategy
-const jwtOptions = {};
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+    secretOrKey: config.secret
+};
 
 // Create JWT strategy
 const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
@@ -19,3 +41,7 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
         }
     });
 });
+
+// Tell passport to use this strategy
+passport.use(jwtLogin);
+passport.use(localLogin);
